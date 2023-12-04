a3d430e30d1fd38e93f01630a04ecc6c13b4ff7d
SmallSpider <568442079@qq.com>
2023-12-04 09:06:59 +0800

    测试用

--- prototype/utils/network.py
+++ prototype/utils/network.py
@@ -80,20 +80,21 @@ if __name__ == "__main__":
 
     def test_server(conn, addr):
         from_client = recvLine(conn)  # 接收客户端发送的数据
+        print(from_client)
         # 进行任意的处理
         sendLine(conn, from_client)  # 返回处理结果给服务器（这里直接返回原数据为例）
         conn.close()
 
     # 服务端监听socket端口
-    PORT = 10001
+    PORT = 44445
     t1 = threading.Thread(target=listen_on_port, args=(test_server, PORT))
     t1.start()
     print(f"listening test server on {PORT} ...")
 
     # 客户端发送请求
-    def handler(conn):
-        sendLine(conn, "aa")  # 发送数据到服务器
-        from_server = recvLine(conn)  # 接收服务器返回的数据
-        return from_server
+    # def handler(conn):
+    #     sendLine(conn, "aa")  # 发送数据到服务器
+    #     from_server = recvLine(conn)  # 接收服务器返回的数据
+    #     return from_server
 
-    print(connect_to(handler, PORT))
+    # print(connect_to(handler, PORT))
